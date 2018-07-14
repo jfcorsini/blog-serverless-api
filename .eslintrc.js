d73fa82d889da804f2355e3db3a18f1f335ad83a
João Corsini <joao.corsini@f-secure.com>
2018-07-14 18:50:14 +0300

    Add dependencies and lint
    Package.json was created with basic dependencies for the application.
    For devDependencies I also added eslint details, so that the .eslintrc.js file can have eslint-config-airbnb linting options.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'script',
+    ecmaFeature: {
+      modules: false,
+    },
+  },
+  rules: {
+    strict: [2, 'global'],
+  },
+};
