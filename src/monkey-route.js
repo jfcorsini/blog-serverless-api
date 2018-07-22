b49e49f2315c45d391fbb1eb9f1fb9eff0bf3182
João Corsini <joao.corsini@f-secure.com>
2018-07-22 21:06:25 +0300

    Add GET and PUT routes
    The routes were created and also added a new policy of dynamodb:UpdateItem to the lambda function to access the table on dynamo.

--- src/monkey-route.js
+++ src/monkey-route.js
@@ -9,6 +9,12 @@ AWS.config.update({
 
 const router = express.Router();
 
+const formatError = (message, err) => ({
+  message,
+  error: err.message,
+  stack: err.stack,
+});
+
 router.post('/', (req, res) => {
   const docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -30,13 +36,43 @@ router.post('/', (req, res) => {
     .then(((data) => {
       res.json(data);
     }))
-    .catch((err) => {
-      res.json({
-        message: 'Failed to create',
-        error: err.message,
-        stack: err.stack,
-      });
-    });
+    .catch(err => res.status(404).json(formatError('Failed create', err)));
+});
+
+router.get('/:id', (req, res) => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: 'monkey_sanctuary',
+    Key: { id: parseInt(req.param('id'), 10) },
+  };
+
+  return docClient.get(params).promise()
+    .then((data) => {
+      if (!data.Item) {
+        return res.status(404).json({ message: 'Id not found' });
+      }
+
+      return res.json(data.Item);
+    })
+    .catch(err => res.status(404).json(formatError('Failed to fetch id', err)));
+});
+
+router.put('/:id', (req, res) => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  const { body: { extra } } = req;
+
+  const params = {
+    TableName: 'monkey_sanctuary',
+    Key: { id: parseInt(req.param('id'), 10) },
+    UpdateExpression: 'set extra = :extra',
+    ExpressionAttributeValues: { ':extra': extra },
+  };
+
+  return docClient.update(params).promise()
+    .then(() => res.status(204).json({}))
+    .catch(err => res.status(404).json(formatError('Failed to update information', err)));
 });
 
 module.exports = router;
